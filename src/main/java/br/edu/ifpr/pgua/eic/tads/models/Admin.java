3359ac58402ce01a8431c247374ed670ce5e1b41
Leo <leonardo.ribeiro250307@gmail.com>
2025-08-28 12:12:39 -0300

    Add Products

--- src/main/java/br/edu/ifpr/pgua/eic/tads/models/Admin.java
+++ src/main/java/br/edu/ifpr/pgua/eic/tads/models/Admin.java
@@ -1,12 +1,18 @@
 package br.edu.ifpr.pgua.eic.tads.models;
 
+import br.edu.ifpr.pgua.eic.tads.App;
+
 public class Admin extends User{
     public Admin(String name, String email) {
         super(name, email);
     }
 
-    private void createProduct(Product product) {
+    private Boolean createProduct(Product product) {
+        if (product == null)
+            return false;
 
+        App.dbProducts.add(product);
+        return true;
     }
 
     private void removeProduct(Product product) {
